43e08171cf11191594efb1eae36b20a444c861aa
Bet GP <57763368+bett-g@users.noreply.github.com>
2022-08-19 20:21:24 +0000

    Documentacion

--- Scripts/StatsUsageWords.sh
+++ Scripts/StatsUsageWords.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+# Enunciado:
 # Indicador estadístico de uso de palabras, deben ser de al menos 4(cuatro) letras.
 # (mostrar un Top Ten de estas palabras ordenadas desde la que tiene más apariciones a la que tiene menos).
 # Es decir, filtrar las palabras que tengan al menos 4 letras y de éstas, elegir las 10 más usadas.
