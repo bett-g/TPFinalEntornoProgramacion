3ff23c85c0f5a3023634f288bc1f6c1fa175b0e5
Bet GP <57763368+bett-g@users.noreply.github.com>
2022-08-19 03:58:52 +0000

    Update menu de opciones.sh

--- menu de opciones.sh
+++ menu de opciones.sh
@@ -1,34 +1,34 @@
-#! /usr/bin/env bash
+#!bin/bash
 submenu(){
-TEXTO=$1
+tex=$1
 echo -ne "
-1) Contador de palabras        4) Contador de oraciones
-2) Contador uso de palabras    5) Contador de lineas en blanco
-3) Identificador de nombres    0) Volver al menú principal
+1) Longitud de palabras        4) Longitud de oraciones
+2) Uso de palabras             5) Contador de lineas en blanco
+3) Nombres propios             0) Volver al menú principal
 C) Limpiar pantalla
-Eliga una opción: "
+Opciones: "
 read opcion
 case $opcion in
-        1) ./Scripts/statsWords.sh $TEXTO ; submenu $TEXTO ;;
-        2) ./Scripts/statsUsageWords.sh $TEXTO ; submenu $TEXTO;;
-        3) ./Scripts/findNames.sh $TEXTO ; submenu $TEXTO;;
-        4) ./Scripts/statsSentences.sh $TEXTO ; submenu $TEXTO;;
-        5) ./Scripts/blankLinesCounter.sh $TEXTO ; submenu $TEXTO;;
+        1) ./Scripts/StatsWords.sh $tex ; submenu $tex ;;
+        2) ./Scripts/StatsUsageWords.sh $tex ; submenu $tex;;
+        3) ./Scripts/findNames.sh $tex ; submenu $tex;;
+        4) ./Scripts/statsSentences.sh $tex ; submenu $tex;;
+        5) ./Scripts/blankLinesCounter.sh $tex ; submenu $tex;;
         0) clear ; menu ;;
-        C) clear ; submenu $TEXTO;;
-        *) echo -e "Opcion incorrecta $opcion" ; submenu $TEXTO;;
+        C) clear ; submenu $tex;;
+        *) echo -e "Error! La opcion no es correcta $opcion" ; submenu $tex;;
 esac
 }
 
 menu(){
-echo "Seleccione un TEXTO de texto por favor."
+echo "Seleccionar un TEXTO de texto"
 ls /texto/
 read
 if [ -r /texto/$REPLY ] && [ -f /texto/$REPLY ]
 then
     submenu /texto/$REPLY
 else
-    echo "No es un archivo de texto simple, no tiene permisos de lectura o no existe."
+    echo "No tiene permisos de lectura o no existe"
 fi
 }
 
