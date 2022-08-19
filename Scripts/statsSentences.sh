0f71ecb8e3900b869d8d9ba455ac81d2951f3b33
Bet GP <57763368+bett-g@users.noreply.github.com>
2022-08-19 20:27:05 +0000

    Documentacion

--- Scripts/statsSentences.sh
+++ Scripts/statsSentences.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# Enunciado:
+# Indicador estadístico de longitud de oraciones (la más corta, la más larga y el promedio de longitud)
 echo "Indicador estadistico de longitud de oraciones"
-echo "----------==----------------------------------------"
+echo "----------------------------------------------------"
 
+# Asignamos el archivo a una variable
 texto=$1
 
-# Se guarda en variable la cantidad de palabras por oracion con wc
+# Se guarda en variable la cantidad de palabras por oracion con wc -w (word)
 palabras=$(wc -w < $texto)
 
-# Se guarda en variable la cantidad de oraciones con wc
+# Se guarda en variable la cantidad de oraciones con wc -l (line)
 oraciones=$(wc -l < $texto)
 
 echo "Cantidad de oraciones: $oraciones, Cantidad de palabras: $palabras"
 echo "----------------------------------------------------"
 
+# Se valida si existen oraciones y palabras entonces se realiza el analisis
 if [[ $oraciones > 0 && $palabras > 0 ]]
 then
 
@@ -21,21 +25,21 @@ then
 	prom=$(echo "scale=2; $palabras / $oraciones" | bc -l)
 
 	# Se lee línea a línea hasta el final del archivo
-        # Se cuenta la cantidad de líneas
 	cat $texto |  while read line
 	do
 
+      # Se cuenta la cantidad de líneas
 	echo $line | wc -w
 	done < $texto >> contenedor.txt 
 
 	# Se ordena por orden numérico y se guarda en el archivo
 	sort -n contenedor.txt > contenedor2.txt
 
-	# Se guarda por variables el primer y último elemento del archivo.
+	# Se guarda por variables el primer y último elemento del archivo
 	min=$(head -1 contenedor2.txt)
 	max=$(tail -1 contenedor2.txt)
 
-	# Se muestra el resultado por salida estandar.
+	# Se visualiza el resultado
 	echo "La oración con mayor longitud tiene $max palabras"
 	echo "La oración con menor longitud tiene $min palabras"
 	echo "El promedio de la longitud es de: $prom"
