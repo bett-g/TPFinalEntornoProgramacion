7129f1f2056fca059ed2cff50ced7f9244b0938c
Bet GP <57763368+bett-g@users.noreply.github.com>
2022-08-19 20:19:37 +0000

    Update findNames.sh

--- Scripts/findNames.sh
+++ Scripts/findNames.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
+# Enunciado:
 # Identificación de nombres propios (se identifican sólo si están en este formato Nnnnnnnnn)
-# aunque la palabra no sea un nombre propio realmente. Ejemplos: Mateo, Estonoesunnombre, Ana. 
+# aunque la palabra no sea un nombre propio realmente. Ejemplos: Mateo, Estonoesunnombre, Ana
 
+# Asignamos el texto a una variable
 texto=$1
-# Se contabiliza mayuscula con wc y se agrega el valor a una variable
+# Se contabiliza mayuscula con wc -l (conteo de lineas) y se agrega el valor a una variable
 mayus=$(grep --color='always' "\<[A-Z][^ ]*\>" $texto | wc -l)
 
-# Si la variable es igual a cero, no hay mayusculas en el archivo de texto, caso contrario se visualizan las palabras resaltada con color.
+# Si la variable es igual a cero, no hay mayusculas en el archivo de texto, caso contrario se visualizan las palabras resaltadas con color
 
 if [[ $mayus = 0 ]]
 then
