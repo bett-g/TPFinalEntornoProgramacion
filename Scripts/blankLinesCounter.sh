b0c9ff3a5afe842fca473dd01e0f9938028c2673
Bet GP <57763368+bett-g@users.noreply.github.com>
2022-08-19 20:44:20 +0000

    Documentacion

--- Scripts/blankLinesCounter.sh
+++ Scripts/blankLinesCounter.sh
@@ -1,12 +1,15 @@
 
 #!/bin/bash
+# Enunciado:
+# Contador de líneas en blanco
+
 echo "Contador de líneas en blanco"
 
 # Se guarda el archivo de texto en una variable
 texto=$1
 
-# Se guarda el nuevo de lineas usando Grep
-cont=$(grep -cvP '\S' $texto)
+# Se guarda el nuevo de lineas usando Grep -cv (c es count y con v contabiliza las lineas que no coinciden con '\S' que significan lineas en blanco)
+cont=$(grep -cv '\S' $texto)
 
 # Se visualiza el resultado
 echo "El archivo de texto tiene $cont lineas en blanco"
